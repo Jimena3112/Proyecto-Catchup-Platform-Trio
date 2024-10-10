40ace3d7f996a42af4ab734e1b852a6905640dad
Jimena Cossio <129307119+Jimena3112@users.noreply.github.com>
2024-10-09 20:58:40 -0500

    Update IFavoriteSourceCommandService.cs

--- Proyecto-Catchup-Platform/Books/Domain/Services/IFavoriteSourceCommandService.cs
+++ Proyecto-Catchup-Platform/Books/Domain/Services/IFavoriteSourceCommandService.cs
@@ -1,6 +1,11 @@
-﻿namespace Proyecto_Catchup_Platform.Books.Domain.Services
+using Proyecto_Catchup_Platform.Books.Domain.Model.Aggregates;
+
+namespace Proyecto_Catchup_Platform.Books.Domain.Services
 {
     public interface IFavoriteSourceCommandService
     {
+        Task<FavoriteSource> Add(FavoriteSource favoriteSource);
+        Task<FavoriteSource?> Update(FavoriteSource favoriteSource);
+        Task<bool> Delete(int id);
     }
 }
