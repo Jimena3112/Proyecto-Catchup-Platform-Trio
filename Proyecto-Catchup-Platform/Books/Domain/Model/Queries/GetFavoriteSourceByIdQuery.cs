70767bd822f3b9a1bae6985769fd6a7ef3d901bf
Jimena Cossio <129307119+Jimena3112@users.noreply.github.com>
2024-10-09 21:54:10 -0500

    Update GetFavoriteSourceByIdQuery.cs

--- Proyecto-Catchup-Platform/Books/Domain/Model/Queries/GetFavoriteSourceByIdQuery.cs
+++ Proyecto-Catchup-Platform/Books/Domain/Model/Queries/GetFavoriteSourceByIdQuery.cs
@@ -1,6 +1,4 @@
 ﻿namespace Proyecto_Catchup_Platform.Books.Domain.Model.Queries
 {
-    public record GetFavoriteSourceByIdQuery(int id)
-    {
-    }
+    public record GetFavoriteSourceByIdQuery(int id);
 }
